0d95a97c9735ffea52e38a1e82259accdecfbe23
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-08-06 22:35:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 function init() {
+  var formTemplate = document.getElementById("recipe-form-template").innerHTML
   //put any page initialization/handlebars initialization here
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+var createRecipe = {
+  description: 'yummy chicken noodle soup',
+  ingredients: [
+    {quantity: "1 cup", name: 'chicken'},
+    {quantity: "3 nanoliters", name: 'stock'},
+    {quantity: "12", name: 'noodles'}
+  ]
+}
+ 
+var template = Handlebars.compile(document.getElementById("my-template").innerHTML);
+var html = template(recipe);
